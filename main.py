8389ba7bd3829cbb171c5c20d70b1172ccd9e358
Johan Vasquez <johanjvc7@gmail.com>
2022-07-01 21:29:43 -0500

    Add modify method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import modules.filemanager as fm
 import modules.dataManager as dm
-
+from os import system
 
 template_info = {
     'productos':[
@@ -29,30 +29,44 @@ def listar_categorias():
     return (cat, llave)
 
 if __name__ == '__main__':
-    fm.RUTA_DATA += 'inventario.json'
-    if fm.check_file():
-        info = fm.Read_file()
-    else:
-        fm.Create_file(template_info)
-    print("""
+    while True: 
+        system('cls')
+        fm.RUTA_DATA += 'inventario.json'
+        if fm.check_file():
+            info = fm.Read_file()
+        else:
+            fm.Create_file(template_info)
+        print("""
 1. Agregar un producto
 2. Buscar un producto
 3. Eliminar un producto
-    """)
-    opcion = int(input(': '))
-    if opcion == 1:
-        filtro = listar_categorias
-        codigo = int(input('Ingrese el codigo: '))
-        nombre = input('Ingrese el nombre del producto: ')
-        cantidad = int(input('Ingrese la cantidad: '))
-        producto = {
-            'codigo': codigo,
-            'nombre' : nombre,
-            'cantidad' : cantidad
-        }
-        fm.add_info('productos', filtro[0], filtro[1], producto)
-    elif opcion == 2:
-        filtro = listar_categorias
-        dm.buscar(info)
-    elif opcion == 3:
-        filtro = listar_categorias
+4. Modificar un producto
+5. Salir
+        """)
+        opcion = int(input(': '))
+        if opcion == 1:
+            filtro = listar_categorias()
+            codigo = str(input('Ingrese el codigo: '))
+            nombre = input('Ingrese el nombre del producto: ')
+            cantidad = int(input('Ingrese la cantidad: '))
+            producto = {
+                'codigo': codigo,
+                'nombre' : nombre,
+                'cantidad' : cantidad
+            }
+            fm.add_info('productos', filtro[0], filtro[1], producto)
+        elif opcion == 2:
+            filtro = listar_categorias()
+            dm.Mostrar_producto(info, filtro, 'productos')
+            input()
+        elif opcion == 3:
+            filtro = listar_categorias()
+            dm.Borrar_producto(info, filtro, 'productos')
+            input()
+        elif opcion == 4:
+            filtro = listar_categorias()
+            dm.Modificar_producto(info, filtro, 'productos')
+            input()
+
+        elif opcion == 5:
+            break
--- modules/dataManager.py
+++ modules/dataManager.py
@@ -1,2 +1,46 @@
-def buscar(lista):
-    print(lista)
+import modules.filemanager as fm
+
+def Buscar(data : dict, filtro : tuple, llave_principal, codigop):
+    Lista_datos = []
+    for item in data[llave_principal]:
+        Lista_datos = item.get(filtro[1], -1)
+        if Lista_datos != -1:
+            break
+    for i, producto in enumerate(Lista_datos):
+        if codigop in producto['codigo']:
+            return i
+
+def Mostrar_producto(data : dict, filtro : tuple, llave_principal):
+    codigo = input('Ingrese el codigo del producto a buscar: ')
+    i = Buscar(data, filtro, llave_principal, codigo)
+    print(f'''
+Codigo : {data[llave_principal][filtro[0]][filtro[1]][i]["codigo"]}
+Nombre : {data[llave_principal][filtro[0]][filtro[1]][i]["nombre"]}
+Cantidad : {data[llave_principal][filtro[0]][filtro[1]][i]["cantidad"]}
+    ''')
+def Borrar_producto(data : dict, filtro : tuple, llave_principal):
+    codigo = input('Ingrese el codigo del producto a borrar: ')
+    i = Buscar(data, filtro, llave_principal, codigo)
+    print(f'''
+Codigo : {data[llave_principal][filtro[0]][filtro[1]][i]["codigo"]}
+Nombre : {data[llave_principal][filtro[0]][filtro[1]][i]["nombre"]}
+Cantidad : {data[llave_principal][filtro[0]][filtro[1]][i]["cantidad"]}
+    ''')
+    data[llave_principal][filtro[0]][filtro[1]].pop(i)
+    print('Registro eliminado correctamente')
+    fm.Create_file(data)
+
+def Modificar_producto(data : dict, filtro : tuple, llave_principal):
+    codigo = input('Ingrese el codigo del producto a modificar: ')
+    i = Buscar(data, filtro, llave_principal, codigo)
+    print(f'''
+Codigo : {data[llave_principal][filtro[0]][filtro[1]][i]["codigo"]}
+Nombre : {data[llave_principal][filtro[0]][filtro[1]][i]["nombre"]}
+Cantidad : {data[llave_principal][filtro[0]][filtro[1]][i]["cantidad"]}
+    ''')
+    data[llave_principal][filtro[0]][filtro[1]][i]["codigo"] = input('Ingrese el nuevo codigo: ')
+    data[llave_principal][filtro[0]][filtro[1]][i]["nombre"] = input('Ingrese el nuevo nombre: ')
+    data[llave_principal][filtro[0]][filtro[1]][i]["cantidad"] = int(input('Actualice la cantidad: '))
+
+    print('Registro a sido actualizado correctamente')
+    fm.Create_file(data)
